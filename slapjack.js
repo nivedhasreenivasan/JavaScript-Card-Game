2043a0d77b7d609a6a942adcf1a46db041669637
Roulette880 <73308413+Roulette880@users.noreply.github.com>
2022-01-31 21:58:27 -0600

    JS framework

--- slapjack.js
+++ slapjack.js
@@ -1,35 +1,61 @@
 // A list of codes for the images and unified nomenclature
-const deck = ['AS', '2S', '3S', '4S', '5S', '6S', '7S', '8S', '9S', '0S', 'JS', 'QS', 
-'KS', 'AD', '2D', '3D', '4D', '5D', '6D', '7D', '8D', '9D', '0D', 'JD', 'QD', 'KD', 
-'AC', '2C', '3C', '4C', '5C', '6C', '7C', '8C', '9C', '0C', 'JC', 'QC', 'KC', 'AH', 
-'2H', '3H', '4H', '5H', '6H', '7H', '8H', '9H', '0H', 'JH', 'QH', 'KH'];
+const mainDeck = ['AS', '2S', '3S', '4S', '5S', '6S', '7S', '8S', '9S', '0S', 'JS', 'QS', 'KS', 
+'AD', '2D', '3D', '4D', '5D', '6D', '7D', '8D', '9D', '0D', 'JD', 'QD', 'KD', 
+'AC', '2C', '3C', '4C', '5C', '6C', '7C', '8C', '9C', '0C', 'JC', 'QC', 'KC', 
+'AH', '2H', '3H', '4H', '5H', '6H', '7H', '8H', '9H', '0H', 'JH', 'QH', 'KH'];
+// ^^ will be used as "cards" because the codes line up with the image file names (e.g. AS -> AS.png)
+
+
 
 function shuffle() {
-    let current_index = deck.length, rIndex;
+    let current_index = mainDeck.length, rIndex;
 
     while(current_index != 0) {
-        rIndex = Math.floor(Math.random() * deck.length);
+        rIndex = Math.floor(Math.random() * mainDeck.length);
         current_index--;
 
-        [deck[current_index], deck[rIndex]] = [deck[rIndex], deck[current_index]];
+        [mainDeck[current_index], mainDeck[rIndex]] = [mainDeck[rIndex], mainDeck[current_index]];
     }
 }
 
-shuffle();
 
 var stack = {
-    curr_card: '',
+    currCard: '',
     deck: []
 }
+// I dont know whether to have an [] of players or just keep the number of players static and named
+var players = [];
+var numP = 4;
 
-var player1 = {
-    cards: []
+//called at the biggining of the game
+function setUp(numPLayers) {
+    shuffle();
+    numP = numPLayers;
+    for(let i = 0; i < numPLayers; i++) {
+        let split = (52 / numPLayers)
+        players[i] = {
+            cards = mainDeck.slice(split * i, split * (i + 1))
+        }
+    }
 }
 
-var computer1 = {
-    cards: []
+//more of a template than anything concrete as per ^^
+function slap(index) {
+    if(stack.currCard.charAt(0) == 'J') {
+        players[index].splice(0, 0, stack.deck);
+        stack.deck = [];
+        stack.currCard = '';
+    } else {
+        players[(index + 1) % numP].cards.splice(0, 0, players[index].pop());
+    }
 }
 
-function playerSlap() {
-    if()
+function placeCard(player) {
+    if(player.cards.length == 0) {
+        // TODO -> Lose 
+    } 
+
+    stack.deck.push(placeCard.cards.pop());
+    stack.currCard = stack.deck[stack.deck.length - 1];
 }
+
